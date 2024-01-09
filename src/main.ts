c90e935dcef32abb9ab102f350412cc46096aad0
maglana <m.srutkowska@gmail.com>
2024-01-09 11:39:43 +0100

    get started and clock

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import clockLogo from "./Logo/clock.png";
+import buttonLogo from "./Logo/get-started.png";
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${buttonLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${clockLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Components</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+    Click on the BUTTON and CLOCK logos to learn more
     </p>
   </div>
 `
