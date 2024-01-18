fdde28b052d71a6503c872fd72bf7498585db443
maglana <m.srutkowska@gmail.com>
2024-01-18 09:52:03 +0100

    finished css and js of all card

---
+++ src/components/changingCards/index.js
@@ -0,0 +1,11 @@
+const fourth = document.querySelector('.fourth');
+
+fourth.addEventListener("mouseover", function(){
+    fourth.classList.add("up-reverse-class2");
+})
+
+const third = document.querySelector('.third');
+
+third.addEventListener("mouseover", function(){
+    third.classList.add("up-reverse-class");
+})
